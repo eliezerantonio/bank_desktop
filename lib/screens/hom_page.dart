9598b82004930f5fec25cda9448254e3e97258f4
goncalveshigino <goncalveshigino20@gmail.com>
2021-05-25 14:55:46 +0100

    home page

--- lib/screens/hom_page.dart
+++ lib/screens/hom_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_desktop/helpers/consts.dart'; 
+import 'package:flutter_desktop/helpers/consts.dart';
+import 'package:flutter_desktop/screens/login_screen.dart'; 
+import 'package:flutter_desktop/util/nav.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -7,8 +9,11 @@ class HomePage extends StatelessWidget {
 
   final size = MediaQuery.of(context).size;
 
+  _onClickLogout() {
+    push(context, LoginScreen(), replace: true);
+  }
+
     return Scaffold(
-     
       body: Row(
         children: [
 
@@ -42,8 +47,73 @@ class HomePage extends StatelessWidget {
                     ],
                   ),
                 ), 
-                SizedBox(height: 40,), 
-                
+                SizedBox(height: 10,),
+                Container(
+                  width: size.width * 0.5,
+                  height: size.height * 0.4,
+                  child: ListView(
+                    children: [
+                     Padding(
+                       padding: const EdgeInsets.all(8.0),
+                       child: Text('Menu', style: TextStyle(fontSize: 16, color: Colors.white54),),
+                     ),
+                      ListTile(
+                        leading: Icon(Icons.star, color: Colors.white,),
+                        title: Text('Cliente',style: TextStyle(color: Colors.white),),
+                        onTap: (){
+                          print("Item 1");
+                          
+                        },
+                      ), 
+                      ListTile(
+                        leading: Icon(Icons.star, color: Colors.white,),
+                        title: Text('Emprestimo',style: TextStyle(color: Colors.white),),
+                        trailing: Icon(Icons.arrow_forward, color: Colors.white,),
+                        onTap: (){
+                          print("Item 1");
+                          
+                        },
+                      ),
+                       ListTile(
+                        leading: Icon(Icons.star, color: Colors.white,),
+                        title: Text('Favoritos',style: TextStyle(color: Colors.white),),
+                        trailing: Icon(Icons.arrow_forward, color: Colors.white,),
+                        onTap: (){
+                          print("Item 1");
+                         
+                        },
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.star, color: Colors.white,),
+                        title: Text('Emprestimo',style: TextStyle(color: Colors.white),),
+                        trailing: Icon(Icons.arrow_forward, color: Colors.white,),
+                        onTap: (){
+                          print("Item 1");
+                          
+                        },
+                      ),
+                    ],
+                  ),
+                ), 
+
+                Spacer(),
+
+                GestureDetector(
+                  onTap:  _onClickLogout,
+                  child: Container(
+                    padding: EdgeInsets.only(right: 30, bottom: 20),
+                    width: size.width,
+                    height: 40,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon( Icons.logout, color: Colors.white,),
+                        SizedBox(width: 10,),
+                        Text('Logout', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),)
+                      ],
+                    ),
+                  ),
+                )
              
               ],
             ),
